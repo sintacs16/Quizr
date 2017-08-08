8336da190a306786d9905edadac3fd61bf2e83ac
Aravind Vasudev <aravindv96@yahoo.com>
2017-08-08 11:18:15 +0530

    Fixed Timer

--- src/clock.js
+++ src/clock.js
@@ -3,10 +3,14 @@ import $ from "jquery";
 //Disable contextmenu
 $(() => $("body").on("contextmenu", e => false));
 
-let timer = 10;
+let timer = 66;
 let x = setInterval(() => {
   if (timer != 0) {
-    $("#quiz-name").text(timer + " seconds left...");
+    let minute = Math.floor(timer / 60);
+    let seconds = timer - (minute * 60);
+    minute = minute < 10 ? `0${minute}` : minute;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+    $("#quiz-name").text(`${minute} Minute ${seconds} Seconds`);
     timer--;
   } else {
     $("#quiz-name").text("oooops....");
